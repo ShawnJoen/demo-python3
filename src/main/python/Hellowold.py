9a6959a740116e602f9af7c7f73e9fd32941256a
quanchunlin <quanchunlin108@163.com>
2018-05-22 19:47:25 +0800

    thread

--- src/main/python/Hellowold.py
+++ src/main/python/Hellowold.py
@@ -1167,35 +1167,35 @@ import time
 #threading.activeCount(): 返回正在运行的线程数量，与len(threading.enumerate())有相同的结果
 #使用 threading 模块创建线程
 #我们可以通过直接从 threading.Thread 继承创建一个新的子类，并实例化后调用 start() 方法启动新线程
-import threading
-import time
-exitFlag = 0
-class myThread (threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-    def run(self):
-        print ("开始线程：" + self.name)
-        print_time(self.name, self.counter, 5)
-        print ("退出线程：" + self.name)
-def print_time(threadName, delay, counter):
-    while counter:
-        if exitFlag:
-            threadName.exit()
-        time.sleep(delay)
-        print ("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
-# 创建新线程
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
-# 开启新线程
-thread1.start()
-thread2.start()
-thread1.join()#等待至线程中止。这阻塞调用线程直至线程的join() 方法被调用中止-正常退出或者抛出未处理的异常-或者是可选的超时发生
-thread2.join()#
-print ("退出主线程")
+# import threading
+# import time
+# exitFlag = 0
+# class myThread (threading.Thread):
+#     def __init__(self, threadID, name, counter):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
+#         self.counter = counter
+#     def run(self):
+#         print ("开始线程：" + self.name)
+#         print_time(self.name, self.counter, 5)
+#         print ("退出线程：" + self.name)
+# def print_time(threadName, delay, counter):
+#     while counter:
+#         if exitFlag:
+#             threadName.exit()
+#         time.sleep(delay)
+#         print ("%s: %s" % (threadName, time.ctime(time.time())))
+#         counter -= 1
+# # 创建新线程
+# thread1 = myThread(1, "Thread-1", 1)
+# thread2 = myThread(2, "Thread-2", 2)
+# # 开启新线程
+# thread1.start()
+# thread2.start()
+# thread1.join()#等待至线程中止。这阻塞调用线程直至线程的join() 方法被调用中止-正常退出或者抛出未处理的异常-或者是可选的超时发生
+# thread2.join()#
+# print ("退出主线程")
 # 开始线程：Thread-1
 # 开始线程：Thread-2
 # Thread-1: Mon May 21 22:42:25 2018
@@ -1210,6 +1210,132 @@ print ("退出主线程")
 # Thread-2: Mon May 21 22:42:32 2018
 # Thread-2: Mon May 21 22:42:34 2018
 # 退出线程：Thread-2
+# 退出主线程
+
+#线程同步
+# Thread 对象的 Lock 和 Rlock 可以实现简单的线程同步
+#将其操作放到 acquire 和 release 方法之间
+# import threading
+# import time
+# class myThread (threading.Thread):
+#     def __init__(self, threadID, name, counter):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
+#         self.counter = counter
+#     def run(self):
+#         print ("开启线程： " + self.name)
+#         # 获取锁，用于线程同步
+#         threadLock.acquire()
+#         print_time(self.name, self.counter, 3)
+#         # 释放锁，开启下一个线程
+#         threadLock.release()
+# def print_time(threadName, delay, counter):
+#     while counter:
+#         time.sleep(delay)
+#         print ("%s: %s" % (threadName, time.ctime(time.time())))
+#         counter -= 1
+# threadLock = threading.Lock()
+# threads = []
+# # 创建新线程
+# thread1 = myThread(1, "Thread-1", 1)
+# thread2 = myThread(2, "Thread-2", 2)
+# # 开启新线程
+# thread1.start()
+# thread2.start()
+# # 添加线程到线程列表
+# threads.append(thread1)
+# threads.append(thread2)
+# # 等待所有线程完成
+# for t in threads:
+#     t.join()
+# print ("退出主线程")
+# 开启线程： Thread-1
+# 开启线程： Thread-2
+# Thread-1: Mon May 21 22:57:32 2018
+# Thread-1: Mon May 21 22:57:33 2018
+# Thread-1: Mon May 21 22:57:34 2018
+# Thread-2: Mon May 21 22:57:36 2018
+# Thread-2: Mon May 21 22:57:38 2018
+# Thread-2: Mon May 21 22:57:40 2018
+# 退出主线程
+
+#线程优先级队列（ Queue）
+#线程安全的队列类，包括FIFO（先入先出)队列Queue，LIFO（后入先出）队列LifoQueue，
+# 和优先级队列 PriorityQueue
+# 这些队列都实现了锁原语，能够在多线程中直接使用，可以使用队列来实现线程间的同步。
+# Queue 模块中的常用方法:
+    # Queue.qsize() 返回队列的大小
+    # Queue.empty() 如果队列为空，返回True,反之False
+    # Queue.full() 如果队列满了，返回True,反之False
+    # Queue.full 与 maxsize 大小对应
+    # Queue.get([block[, timeout]])获取队列，timeout等待时间
+    # Queue.get_nowait() 相当Queue.get(False)
+    # Queue.put(item) 写入队列，timeout等待时间
+    # Queue.put_nowait(item) 相当Queue.put(item, False)
+    # Queue.task_done() 在完成一项工作之后，Queue.task_done()函数向任务已经完成的队列发送一个信号
+    # Queue.join() 实际上意味着等到队列为空，再执行别的操作
+import queue
+import threading
+import time
+exitFlag = 0
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.q = q
+    def run(self):
+        print ("开启线程：" + self.name)
+        process_data(self.name, self.q)
+        print ("退出线程：" + self.name)
+def process_data(threadName, q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            print ("%s processing %s" % (threadName, data))
+        else:
+            queueLock.release()
+        time.sleep(1)
+threadList = ["Thread-1", "Thread-2", "Thread-3"]
+nameList = ["One", "Two", "Three", "Four", "Five"]
+queueLock = threading.Lock()
+workQueue = queue.Queue(10)
+threads = []
+threadID = 1
+# 创建新线程
+for tName in threadList:
+    thread = myThread(threadID, tName, workQueue)
+    thread.start()
+    threads.append(thread)
+    threadID += 1
+# 填充队列
+queueLock.acquire()
+for word in nameList:
+    workQueue.put(word)
+queueLock.release()
+# 等待队列清空
+while not workQueue.empty():
+    pass
+# 通知线程是时候退出
+exitFlag = 1
+# 等待所有线程完成
+for t in threads:
+    t.join()
+print ("退出主线程")
+# 开启线程：Thread-1
+# 开启线程：Thread-2
+# 开启线程：Thread-3
+# Thread-2 processing One
+# Thread-1 processing Two
+# Thread-3 processing Three
+# Thread-1 processing Four
+# Thread-3 processing Five
+# 退出线程：Thread-2
+# 退出线程：Thread-3
+# 退出线程：Thread-1
 # 退出主线程
 
 #---------------------------------
