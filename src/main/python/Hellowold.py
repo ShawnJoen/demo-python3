be47161b4fca05e6e35a4b0f279bbb206a550e6a
quanchunlin <quanchunlin108@163.com>
2018-05-24 22:10:03 +0800

    JSON 数据解析

--- src/main/python/Hellowold.py
+++ src/main/python/Hellowold.py
@@ -1468,11 +1468,57 @@ for movie in movies:
 # Format: VHS
 # Rating: PG
 # Description: Viewable boredom
+#---------------------------------JSON 数据解析
+# json.dumps(): 对数据进行编码
+# json.loads(): 对数据进行解码
+# Python  编码为 JSON 类型转换对应表：
+# Python	                                JSON
+# dict	                                    object
+# list, tuple	                            array
+# str	                                    string
+# int, float, int- & float-derived Enums	number
+# True	                                    true
+# False	                                    false
+# None	                                    null
+# JSON 解码为 Python 类型转换对应表：
+# JSON	            Python
+# object	        dict
+# array	            list
+# string	        str
+# number (int)	    int
+# number (real)	    float
+# true	            True
+# false	            False
+# null	            None
+
+import json
+# Python 字典类型转换为 JSON 对象
+data = {
+    'no' : 1,
+    'name' : 'Shawn',
+    'url' : 'http://www.Shawn.com'
+}
+json_str = json.dumps(data)#对数据进行编码
+print ("Python 原始数据：", repr(data))
+#Python 原始数据： {'no': 1, 'name': 'Shawn', 'url': 'http://www.Shawn.com'}
+print ("JSON 对象：", json_str)
+#JSON 对象： {"no": 1, "name": "Shawn", "url": "http://www.Shawn.com"}
+
+# Python 字典类型转换为 JSON 对象
+# 将 JSON 对象转换为 Python 字典
+data2 = json.loads(json_str)
+print ("data2['name']: ", data2['name'])#data2['name']:  Shawn
+print ("data2['url']: ", data2['url'])#data2['url']:  http://www.Shawn.com
+#从文件解析 -  json.dump() 和 json.load() 来编码和解码JSON数据
+# 写入 JSON 数据
+# with open('data.json', 'w') as f:
+#     json.dump(data, f)
+# # 读取数据
+# with open('data.json', 'r') as f:
+#     data = json.load(f)
+
+
 #---------------------------------
-
-
-
-
 
 
 
